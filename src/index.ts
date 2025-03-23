582c6a932e54ad9ea09db4b0260d727ca9c927de
Corin Mulliss <corin.mulliss@gmail.com>
2025-03-23 09:59:30 +0000

    refactor: tidied up tables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import path from "path";
 import * as dotenv from "dotenv";
-import CliTable3 from "cli-table3";
 
 import { fetchAircraftData } from "./fetchAircraft";
 import { fetchInterestingAircraft } from "./fetchInterestingAircraft";
-import { summariseAircraft } from "./summary";
+import {
+  createTableOfAircraftByInteresting,
+  createTableOfAircraftByMakeModel,
+  createTableOfAircraftByOwner,
+  summariseAircraft,
+} from "./summary";
 
 dotenv.config();
 
@@ -31,49 +35,13 @@ const getAircraftData = async () => {
 
   const { byOwner, byMake } = summariseAircraft(aircraftData);
 
-  // Owner
-  const table = new CliTable3({
-    head: ["Owner", "Count"],
-  });
-  for (const [owner, count] of Object.entries(byOwner)) {
-    table.push([owner, count]);
-  }
-  table.sort((a, b) => {
-    const a2 = a as [string, number];
-    const b2 = b as [string, number];
-    return b2[1] - a2[1];
-  });
-  table.push(["Total", aircraftData.length]);
-  console.log(table.toString());
-
-  // Make and model
-  const table2 = new CliTable3({
-    head: ["Make & Model", "Count"],
-  });
-  for (const [makeModel, count] of Object.entries(byMake)) {
-    table2.push([makeModel, count]);
-  }
-  table2.sort((a, b) => {
-    const a2 = a as [string, number];
-    const b2 = b as [string, number];
-    return b2[1] - a2[1];
-  });
-  table2.push(["Total", aircraftData.length]);
-  console.log(table2.toString());
-
-  // Type
-  const table3 = new CliTable3({
-    head: ["Type", "Count"],
-  });
-
-  table3.push(["Emergency", aircraftData.filter((a) => a.isEmergency).length]);
-  table3.push(["Military", aircraftData.filter((a) => a.isMilitary).length]);
-  table3.push([
-    "Interesting",
-    aircraftData.filter((a) => a.isInteresting).length,
-  ]);
-
-  console.log(table3.toString());
+  console.log(
+    createTableOfAircraftByOwner(byOwner, aircraftData.length).toString()
+  );
+  console.log(
+    createTableOfAircraftByMakeModel(byMake, aircraftData.length).toString()
+  );
+  console.log(createTableOfAircraftByInteresting(aircraftData).toString());
 
   // TODO: Trigger notification if emergency / military / interesting present
 };
--- src/summary.ts
+++ src/summary.ts
@@ -1,3 +1,4 @@
+import CliTable3 from "cli-table3";
 import { EnrichedAircraft } from "./Aircraft";
 
 type OwnerToCount = {
@@ -26,4 +27,58 @@ export const summariseAircraft = (aircraftData: EnrichedAircraft[]) => {
   }, {});
 
   return { byOwner, byMake };
+};
+
+export const createTableOfAircraftByOwner = (
+  byOwner: OwnerToCount,
+  total: number
+) => {
+  const table = new CliTable3({
+    head: ["Owner", "Count"],
+  });
+  for (const [owner, count] of Object.entries(byOwner)) {
+    table.push([owner, count]);
+  }
+  table.sort((a, b) => {
+    const a2 = a as [string, number];
+    const b2 = b as [string, number];
+    return b2[1] - a2[1];
+  });
+  table.push(["Total", total]);
+  return table;
+};
+
+export const createTableOfAircraftByMakeModel = (
+  byMakeModel: MakeModelToCount,
+  total: number
+) => {
+  const table = new CliTable3({
+    head: ["Make & Model", "Count"],
+  });
+  for (const [makeModel, count] of Object.entries(byMakeModel)) {
+    table.push([makeModel, count]);
+  }
+  table.sort((a, b) => {
+    const a2 = a as [string, number];
+    const b2 = b as [string, number];
+    return b2[1] - a2[1];
+  });
+  table.push(["Total", total]);
+  return table;
+};
+
+export const createTableOfAircraftByInteresting = (
+  aircraftData: EnrichedAircraft[]
+) => {
+  const table = new CliTable3({
+    head: ["Type", "Count"],
+  });
+
+  table.push(["Emergency", aircraftData.filter((a) => a.isEmergency).length]);
+  table.push(["Military", aircraftData.filter((a) => a.isMilitary).length]);
+  table.push([
+    "Interesting",
+    aircraftData.filter((a) => a.isInteresting).length,
+  ]);
+  return table;
 };
